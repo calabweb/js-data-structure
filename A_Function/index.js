36771f83a006a73ab5afc030a323d36cd5cf8b26
Calab Maboya <129751012+calabweb@users.noreply.github.com>
2023-04-10 11:35:17 +0200

    new change

---
+++ A_Function/index.js
@@ -0,0 +1,6 @@
+ //this is my first java script code
+ console.log('calab Maboya')
+ function calab Maboya
+ let = 1 + 00
+ return Result()
+ 
---
+++ A_Function/script.js
@@ -0,0 +1,13 @@
+// function Statement 
+
+function add(){
+let a =5;
+let b =10;
+}
+
+
+
+
+
+
+// function expession
